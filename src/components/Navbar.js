4b5b2161b96a5a71b137af835a8c2e623010d7fa
Engr Saddam Zardari <41242658+engrzardari@users.noreply.github.com>
2021-10-26 09:54:20 -0700

    Nav bar changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+
 export default function Navbar(props) {
     return (
         <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
