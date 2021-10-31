784db9cc050e56250d3f56b22da90d145d1984a3
Engr Saddam Zardari <41242658+engrzardari@users.noreply.github.com>
2021-10-31 05:04:47 -0700

    Routing

--- src/App.js
+++ src/App.js
@@ -1,29 +1,70 @@
 import React, {useState} from 'react'
 import './App.css';
 import About from './components/About';
+import Alert from './components/Alert';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   const[mode,setMode] = useState('light');
+  const[alert,setAlert] = useState(null);
 
   const toggleMode =()=>{
     if(mode==='light'){
       setMode('dark')
+      showAlert('Dark Mode has been activated.', 'success ');
+      document.title='TextUtils - Dark Mode'
     }
     else
     {
       setMode('light')
+      showAlert('Light Mode has been activated.', 'success ')
+      document.title='TextUtils - Home'
     }
+    // setInterval(() => {
+    //   document.title='TextUtils is an Amazing App'
+    // }, 2000);
+
+    // setInterval(() => {
+    //   document.title='TextUtils - Home'
+    // }, 1500);
+
 
   }
+  // to show alert 
+  const showAlert =(message, type)=>{
+    setAlert({
+    msg:message,
+    type : type  
+    })
+    setTimeout(()=>{ 
+      setAlert(null)
+    }, 5000);
+  }
+
 
   return (
     <>
-      <Navbar title='TextUtils' aboutSite='About TextUtils' mode={mode} toggleMode={toggleMode}/>
-      {<TextForm heading="Enter your text here" mode={mode}/>}
-      {/* <About/> */}
+      <Router>
+      <Navbar title='TextUtils' aboutSite='About' mode={mode} toggleMode={toggleMode}/>
+      <Alert alert={alert}/>
+      
+        <Switch>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/">
+            <TextForm showAlert={showAlert} heading="Enter your text here" mode={mode} toggleMode={toggleMode}/>
+          </Route>
+        </Switch>
+      </Router>
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -7,13 +7,18 @@ export default function TextForm(props) {
 
     const handleUpClick=()=>{
         //console.log('Handle on Click' + text);
-        let newtext = text.toUpperCase()    ;
-        setText(newtext );
+        if(text!=''){
+            let newtext = text.toUpperCase()    ;
+            setText(newtext );
+            props.showAlert('Text is converted to Upper case','success ')
+        }
     }
+
     const handleLlowerClick=()=>{
         //console.log('Handle on Click' + text);
-        let newtext = text.toLocaleLowerCase()    ;
+        let newtext = text.toLocaleLowerCase();
         setText(newtext );
+        props.showAlert('Text is converted to Lower case','success ')
     }
 
     const handlePretifyClick=()=>{
@@ -21,12 +26,14 @@ export default function TextForm(props) {
         // let newtext = text.replace("}",/\r/+'} ');
         let newtext = text.replace("}",/\r\n/);
         setText(newtext);
+        props.showAlert('Text is pretified','success ')
     }
 
     const handleMinifyClick=()=>{
         //console.log('Handle on Click' + text);
         let newtext = text.replace(/\n/g, "").replace(/\n/g, "");
         setText(newtext );
+        props.showAlert('Text is minified','success ')
     }
 
     const handleOnChange=(event)=>{
@@ -38,19 +45,28 @@ export default function TextForm(props) {
         //console.log('Handle on Click' + text);
         let newtext = '';
         setText(newtext );
+        props.showAlert('Text is cleared ','danger ')
     }
+    const handleRedundancy = ()=> { 
+        let set1 = new Set(text.split(" "));          
+        let newText = Array.from(set1).join(" ");
+        setText(newText);
+        props.showAlert('Duplication is removed from your text','success ')
+   }
 
 
     return (
         <>
-        <div className={`container my-3 text-${props.mode==='light'?'dark':'light'}`}>
+        <div className={`container my-3 bg-${props.mode==='dark'?'light':'light'}`}>
             <div className="mb-3">
-                <label htmlFor="myBox" className="form-label">{props.heading}</label>
+                <label htmlFor="myBox" className={`form-label text-${props.mode==='light'?'dark':'light'}`}>{props.heading}</label>
                 <textarea placeholder ="Enter text here.." onChange={handleOnChange} className="form-control" id="myBox" rows="8" value={text}></textarea>
             </div>
             <button className="btn btn-outline-success" onClick={handleUpClick}>Convter to Uppercase</button>
             <button className="btn btn-outline-success mx-2" onClick={handleLlowerClick}>Convter to LowerCase</button>
             <button className="btn btn-outline-success mx-2" onClick={handleMinifyClick}>Minify CSS</button>
+            <button className="btn btn-outline-success mx-2" onClick={handleRedundancy}>Remove Duplication</button>
+            <button className="btn btn-outline-success mx-2" onClick={handleRedundancy}>Remove Extra Spaces</button>
             <button className="btn btn-outline-success mx-2" onClick={handleClear}>Clear Text</button>
         </div>
         <div className="container my-3">
